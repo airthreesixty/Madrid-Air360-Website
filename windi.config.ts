3f5fa551d5ada92c72d6f7fd41ec855f4da6772f
koki <koki.takahashi@scalefast.com>
2023-03-15 09:18:30 +0900

    Add the font

--- windi.config.ts
+++ windi.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'windicss/helpers'
 // import colors from 'windicss/colors'
 
 export default defineConfig({
-  plugins: [require('windicss/plugin/typography')]
+  plugins: [require('windicss/plugin/typography')],
+  theme: {
+    fontFamily: {
+      sans: [
+        'Montserrat',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'sans-serif'
+      ]
+    }
+  }
 })
