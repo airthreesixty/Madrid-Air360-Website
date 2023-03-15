4f61e9c8eadb7a17e4836dab31d8308043cd909f
koki <koki.takahashi@scalefast.com>
2023-03-15 10:56:28 +0900

    create the protorype and add the custom colors

--- windi.config.ts
+++ windi.config.ts
@@ -12,6 +12,20 @@ export default defineConfig({
         '-apple-system',
         'sans-serif'
       ]
+    },
+    extend: {
+      colors: {
+        primary: {
+          600: '#056AFE'
+        },
+        secondary: {
+          600: '#6278DF'
+        },
+        black: {
+          600: '#6F727B',
+          700: '#232E4A'
+        }
+      }
     }
   }
 })
