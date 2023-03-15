b411737ac5594ae48b236a57c8d1ef8910ab038b
koki <koki.takahashi@scalefast.com>
2023-03-15 12:09:34 +0900

    Add strapi package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-windicss']
+  modules: ['nuxt-windicss', '@nuxtjs/strapi'],
+  strapi: {
+    url: 'http://localhost:1337'
+  }
 })
