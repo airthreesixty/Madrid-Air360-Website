24153e5bcd987eee3f6451a5ec6fd73e5371db5d
koki <koki.takahashi@scalefast.com>
2023-03-16 10:44:55 +0900

    Add html attribute

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-windicss', '@nuxtjs/strapi', '@nuxtjs/apollo'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
   strapi: {
     url: 'http://localhost:1337'
   },
