de4243bb6b772f6182664c650437cf9124617422
koki <koki.takahashi@scalefast.com>
2023-03-17 14:21:21 +0900

    Test

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-windicss', '@nuxtjs/strapi', '@nuxtjs/apollo'],
+  buildDir: 'frontend/.nuxt',
   app: {
     head: {
       htmlAttrs: {
