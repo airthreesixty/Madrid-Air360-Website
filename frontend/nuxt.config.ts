e0f31e97c529262a912e531523e65ecbe740554d
koki <koki.takahashi@scalefast.com>
2023-03-17 15:56:12 +0900

    Install nuxt-img

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-windicss', '@nuxtjs/strapi', '@nuxtjs/apollo'],
+  modules: ['nuxt-windicss', '@nuxtjs/strapi', '@nuxtjs/apollo', '@nuxt/image-edge'],
   app: {
     head: {
       htmlAttrs: {
