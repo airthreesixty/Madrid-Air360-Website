0c279e1d5711d739f29c007b69276b3d1785b27a
koki <koki.takahashi@scalefast.com>
2023-04-08 16:04:34 +0900

    Adjust the image size

--- client/components/SingleDescription.vue
+++ client/components/SingleDescription.vue
@@ -2,7 +2,7 @@
   <section class="bg-white dark:bg-gray-900" :style="{backgroundImage: backgroundImage ? `url(${backgroundImage})`: '' }">
     <div class="grid px-5 pt-12 pb-6 max-w-screen-[1400px] mx-auto md:gap-8 lg:gap-12 md:py-38 md:grid-cols-12">
       <div class="flex justify-start mb-10 md:col-span-5 md:items-center" :class="{'md:hidden': rightImage}">
-        <nuxt-img :src="props.productImage" alt="mockup" format="webp" class="h-40 lg:w-120 lg:h-70" />
+        <nuxt-img :src="props.productImage" alt="mockup" format="webp" class="h-40 lg:w-96 lg:h-54 xl:w-112 xl:h-63" />
       </div>
       <div class="mr-auto md:mx-auto place-self-center md:col-span-7 lg:pr-6">
         <h1 class="text-white mb-4 pr-4 text-2xl md:text-4xl tracking-wide font-bold dark:text-white" :class="textTitleColor">
@@ -14,7 +14,7 @@
         <div class="max-w-2xl mb-6 font-light tracking-wide text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400" :class="textDescriptionColor" v-html="description" />
       </div>
       <div v-if="rightImage" class="flex hidden md:flex justify-start mb-10 md:block md:col-span-5 md:items-center">
-        <nuxt-img :src="props.productImage" alt="mockup" format="webp" class="h-40 lg:w-120 lg:h-70" />
+        <nuxt-img :src="props.productImage" alt="mockup" format="webp" class="h-40 lg:w-96 lg:h-54 xl:w-112 xl:h-63" />
       </div>
     </div>
   </section>
